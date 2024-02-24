a3626a610566c23eea5b30ad8dd244f093fe9887
VuVietDuy <vuvietduy1234@gmail.com>
2024-02-24 20:41:20 +0700

    Update

--- src/app/simple_signup_form/page.tsx
+++ src/app/simple_signup_form/page.tsx
@@ -1,74 +1,63 @@
 "use client";
-import { useFormik } from "formik";
-
-interface IValues {
-  email: string;
-  password: string;
-}
-
-const validate = (values: IValues) => {
-  const errors: IValues = {
-    email: "",
-    password: "",
-  };
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  if (!values.password) {
-    errors.password = "Required";
-  } else if (values.password.length > 8) {
-    errors.password = "Must be less than 8 characters";
-  }
-};
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 export default function SignupForm() {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
+ const initialValues = { email: '', password: '' }
+
+  const handleSubmit = (data: any): void => {
+    console.log('data', data)
+  }
+
+  const SignupSchema = Yup.object().shape({
+    email: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    password: Yup.string()
+      .min(6, 'Too Short!')
+      .max(10, 'Too Long!')
+      .required('Required'),
   });
 
   return (
     <div className="flex justify-center">
       <div className="card">
-        <form onSubmit={formik.handleSubmit} className="space-y-4">
-          <div className="form-item">
-            <label htmlFor="email" className="label">
-              Email Address
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              className="input"
-            ></input>
-          </div>
-          <div className="space-y-4">
-            <label htmlFor="password" className="label">
-              Password
-            </label>
-            <input
-              // id="password"
-              name="password"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              className="input"
-            ></input>
-          </div>
-          <button type="submit" className="btn">
-            Submit
-          </button>
-        </form>
+      <Formik
+       initialValues={initialValues}
+       validationSchema={SignupSchema}
+       onSubmit={handleSubmit}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             name="email"
+             className="input"
+             onChange={handleChange}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             name="password"
+             className="input"
+             onChange={handleChange}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button className="btn" type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
       </div>
     </div>
   );
